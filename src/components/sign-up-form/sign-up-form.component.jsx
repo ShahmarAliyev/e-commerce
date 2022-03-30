049537722845d634ecd965d0da8b034c027d352f
ShahmarALiyev <saliyev2019@gmail.com>
2022-03-30 15:04:28 -0500

    created context state management tool, finished up authentication features

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,12 +1,13 @@
 import "./sign-up-form.styles.scss";
 
-import { useState } from "react";
+import { useContext, useState } from "react";
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+import { UserContext } from "../contexts/user.context";
 
 const defaultFormFields = {
   displayName: "",
@@ -18,6 +19,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext);
 
   const resetFormField = () => {
     setFormFields(defaultFormFields);
@@ -36,8 +39,7 @@ const SignUpForm = () => {
         email,
         password
       );
-      console.log(user);
-
+      await setCurrentUser(user);
       await createUserDocumentFromAuth(user, { displayName });
       console.log("user created");
       resetFormField();
