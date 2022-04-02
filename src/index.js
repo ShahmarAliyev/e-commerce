d1c57885859102c5d57439bcb67309add73d7ff2
ShahmarALiyev <saliyev2019@gmail.com>
2022-04-02 13:16:45 -0500

    teset

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./components/contexts/user.context";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
