eba806f92712a15a8768a99f6bee189a739ae1e2
ShahmarALiyev <saliyev2019@gmail.com>
2022-04-15 18:50:53 -0500

    Adding redux persis and redux dev tools to our app

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Fragment } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/crown (1).svg";
 
@@ -61,63 +61,3 @@ const Navigation = () => {
 };
 
 export default Navigation;
-
-// import { Link, Outlet } from "react-router-dom";
-// import { Fragment, useContext } from "react";
-
-// import { ReactComponent as Logo } from "../../assets/crown (1).svg";
-
-// import "./navigation.styles.scss";
-// import { logOut } from "../../utils/firebase/firebase.utils";
-// import { UserContext } from "../../components/contexts/user.context";
-// import CartIcon from "../../components/cart-icon/cart-icon.component";
-// import CartDrowdown from "../../components/cart-dropdown/cart-dropdown.component";
-// import { CartDrowdownContext } from "../../components/contexts/cart-dropdown.context";
-
-// const Navigation = () => {
-//   const { isHidden, setHidden } = useContext(CartDrowdownContext);
-//   const { currentUser } = useContext(UserContext);
-
-//   const signUserOut = async () => {
-//     console.log("signin user out ");
-//     await logOut();
-//     console.log("signed out");
-//   };
-
-//   const handleDropdownClick = () => {
-//     setHidden(!isHidden);
-//   };
-//   return (
-//     <Fragment>
-//       <div className="navigation">
-//         <Link className="logo-container" to="/">
-//           <Logo className="logo" />
-//         </Link>
-//         <div className="nav-links-container">
-//           <Link className="nav-link" to="/shop">
-//             Shop
-//           </Link>
-
-//           {currentUser ? (
-//             <span className="nav-link" onClick={signUserOut}>
-//               Sign out
-//             </span>
-//           ) : (
-//             <Link className="nav-link" to="/auth">
-//               Sign in
-//             </Link>
-//           )}
-//           <CartIcon />
-//         </div>
-//         {isHidden && (
-//           <span onClick={handleDropdownClick}>
-//             <CartDrowdown />
-//           </span>
-//         )}
-//       </div>
-//       <Outlet />
-//     </Fragment>
-//   );
-// };
-
-// export default Navigation;
