1c672c48ee020388ca7fd473fff3daf0b95bdb86
ShahmarALiyev <saliyev2019@gmail.com>
2022-05-12 18:45:40 -0500

    finsihing create payment intent functions

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -13,12 +13,38 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 }),
+    }).then((res) => res.json());
+    const {
+      paymentIntent: { client_secret },
+    } = response;
+
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: "TestName",
+        },
+      },
+    });
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === "succeeded") {
+        alert("Payment Successful");
+      }
+    }
   };
 
-  console.log(React.version);
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay Now</Button>
         <CardElement />
