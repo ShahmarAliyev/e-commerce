1ebf661c82d40ca098b1349cec74ef7ffc299089
ShahmarALiyev <saliyev2019@gmail.com>
2022-05-15 19:12:31 -0500

    converting cart redux to typescript

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,35 @@
+import { setCarItems, setIsCartOpen } from "./cart.actions";
+import { AnyAction } from "redux";
+import { CartItem } from "./cart.types";
+
+export type CartState = {
+  isCartOpen: boolean;
+  cartItems: CartItem[];
+  cartCount: number;
+  cartTotal: number;
+};
+export const INITIAL_STATE: CartState = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+};
+
+const cartReducer = (state = INITIAL_STATE, action: AnyAction): CartState => {
+  if (setIsCartOpen.match(action)) {
+    return {
+      ...state,
+      isCartOpen: action.payload,
+    };
+  }
+
+  if (setCarItems.match(action)) {
+    return {
+      ...state,
+      cartItems: action.payload,
+    };
+  }
+  return state;
+};
+
+export default cartReducer;
--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,6 +1,7 @@
+import { CartState } from "./cart.reducer";
 import { createSelector } from "reselect";
 
-const selectCartReducer = (state) => state.cart;
+const selectCartReducer = (state): CartState => state.cart;
 
 export const selectCartItems = createSelector(
   [selectCartReducer],
